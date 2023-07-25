c6f5f4c77ace77f2f715913e0c9a5fb8f1c265ef
Denzel88 <32003488+Denzel88@users.noreply.github.com>
2023-07-25 14:59:13 -0600

    Primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+// Nav hamcolapscolaps selections
+const colaps = document.querySelector("#colaps-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+// Scroll to top selection
+const scrollUp = document.querySelector("#scroll-up");
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+// Hamcolaps menu function
+colaps.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+// Close hamcolaps menu when a link is clicked
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
+// scroll to top functionality
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
